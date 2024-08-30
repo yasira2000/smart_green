1b6ade30cfa2acc0abddd7134cb1d91d1bb8ae5c
yasira2000 <yasirapunsith@gmail.com>
2024-08-30 13:16:51 +0530

    add crop image functionalities

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@
 // import 'package:image_picker/image_picker.dart';
 // import 'package:http/http.dart' as http;
 // import 'dart:io';
+// import 'dart:convert'; // For JSON decoding
 
 // void main() {
 //   runApp(MyApp());
@@ -25,6 +26,7 @@
 // class _PhotoCaptureScreenState extends State<PhotoCaptureScreen> {
 //   File? _image;
 //   String? _uploadedFileUrl;
+//   String? _prediction; // Variable to store predictions
 
 //   Future<void> _pickImage() async {
 //     final pickedFile =
@@ -42,7 +44,7 @@
 //       print("No image selected.");
 //       return;
 //     } else {
-//       print("image selected");
+//       print("Image selected.");
 //     }
 
 //     try {
@@ -57,25 +59,22 @@
 //       final response = await request.send();
 //       final responseData = await response.stream.bytesToString();
 
-//       print(response.statusCode);
-//       print(responseData);
-//       print(response);
-
 //       if (response.statusCode == 200) {
-//         print("upload success");
-//         final filename = responseData.contains('filename')
-//             ? responseData.split('filename":"')[1].split('"')[0]
-//             : null;
+//         print("Upload success");
+//         print(response);
 
-//         if (filename != null) {
-//           setState(() {
-//             _uploadedFileUrl =
-//                 'http://rasanjali-smart-green-web.hf.space//uploads/$filename';
-//           });
-//           print('Uploaded successfully');
-//         } else {
-//           print('Filename extraction failed');
-//         }
+//         // Decode the JSON response
+//         final Map<String, dynamic> jsonResponse = json.decode(responseData);
+//         print(jsonResponse);
+
+//         // Extract the filename and prediction
+//         final String prediction = jsonResponse['prediction'] as String;
+
+//         setState(() {
+//           _uploadedFileUrl =
+//               'http://rasanjali-smart-green-web.hf.space/uploads/${jsonResponse['filename']}';
+//           _prediction = prediction;
+//         });
 //       } else {
 //         print('Upload failed');
 //       }
@@ -87,27 +86,43 @@
 //   @override
 //   Widget build(BuildContext context) {
 //     return Scaffold(
-//       appBar: AppBar(title: Text('Capture & Upload Photo')),
+//       appBar: AppBar(
+//           title: Center(
+//         child: Text(
+//           'Smart Green',
+//           textAlign: TextAlign.center,
+//         ),
+//       )),
 //       body: Center(
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: [
-//             _image == null ? Text('No image selected.') : Image.file(_image!),
-//             SizedBox(height: 20),
-//             ElevatedButton(
-//               onPressed: _pickImage,
-//               child: Text('Capture Photo'),
-//             ),
-//             SizedBox(height: 10),
-//             ElevatedButton(
-//               onPressed: _uploadImage,
-//               child: Text('Upload Photo'),
-//             ),
-//             SizedBox(height: 20),
-//             _uploadedFileUrl != null
-//                 ? Image.network(_uploadedFileUrl!)
-//                 : Text('No image uploaded.'),
-//           ],
+//         child: SingleChildScrollView(
+//           child: Column(
+//             mainAxisAlignment: MainAxisAlignment.center,
+//             children: [
+//               _image == null ? Text('No image selected.') : Image.file(_image!),
+//               SizedBox(height: 20),
+//               ElevatedButton(
+//                 onPressed: _pickImage,
+//                 child: Text('Capture Photo'),
+//               ),
+//               SizedBox(height: 10),
+//               ElevatedButton(
+//                 onPressed: _uploadImage,
+//                 child: Text('Upload Photo'),
+//               ),
+//               SizedBox(height: 20),
+//               _uploadedFileUrl != null
+//                   // ? Image.network(_uploadedFileUrl!)
+//                   ? Text("")
+//                   : Text('No image uploaded.'),
+//               SizedBox(height: 20),
+//               _prediction != null
+//                   ? SizedBox(
+//                       height: 100,
+//                       child: Text('Prediction: $_prediction'),
+//                     )
+//                   : Text('No predictions available.'),
+//             ],
+//           ),
 //         ),
 //       ),
 //     );
@@ -116,9 +131,10 @@
 
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:image_cropper/image_cropper.dart';
 import 'package:http/http.dart' as http;
 import 'dart:io';
-import 'dart:convert'; // For JSON decoding
+import 'dart:convert';
 
 void main() {
   runApp(MyApp());
@@ -142,17 +158,75 @@ class PhotoCaptureScreen extends StatefulWidget {
 class _PhotoCaptureScreenState extends State<PhotoCaptureScreen> {
   File? _image;
   String? _uploadedFileUrl;
-  String? _prediction; // Variable to store predictions
+  String? _prediction;
 
   Future<void> _pickImage() async {
     final pickedFile =
         await ImagePicker().pickImage(source: ImageSource.camera);
 
     if (pickedFile != null) {
-      setState(() {
-        _image = File(pickedFile.path);
-      });
+      File? croppedFile = await _cropImage(File(pickedFile.path));
+
+      if (croppedFile != null) {
+        setState(() {
+          _image = croppedFile;
+        });
+      }
     }
+  }
+
+  // Future<File?> _cropImage(File imageFile) async {
+  //   CroppedFile? croppedFile = await ImageCropper().cropImage(
+  //     sourcePath: imageFile.path,
+  //     aspectRatioPresets: [
+  //       CropAspectRatioPreset.square,
+  //       CropAspectRatioPreset.ratio3x2,
+  //       CropAspectRatioPreset.original,
+  //       CropAspectRatioPreset.ratio4x3,
+  //       CropAspectRatioPreset.ratio16x9,
+  //     ],
+  //     androidUiSettings: AndroidUiSettings(
+  //       toolbarTitle: 'Crop Image',
+  //       toolbarColor: Colors.deepOrange,
+  //       toolbarWidgetColor: Colors.white,
+  //       initAspectRatio: CropAspectRatioPreset.original,
+  //       lockAspectRatio: false,
+  //     ),
+  //     iosUiSettings: IOSUiSettings(
+  //       minimumAspectRatio: 1.0,
+  //     ),
+  //   );
+
+  //   if (croppedFile != null) {
+  //     return File(croppedFile.path);
+  //   }
+  //   return null;
+  // }
+
+  Future<File?> _cropImage(File imageFile) async {
+    CroppedFile? croppedFile = await ImageCropper().cropImage(
+      sourcePath: imageFile.path,
+      aspectRatio: CropAspectRatio(
+          ratioX: 1.0,
+          ratioY: 1.0), // Replace aspectRatioPresets with aspectRatio if needed
+      uiSettings: [
+        AndroidUiSettings(
+          toolbarTitle: 'Crop Image',
+          toolbarColor: Colors.deepOrange,
+          toolbarWidgetColor: Colors.white,
+          initAspectRatio: CropAspectRatioPreset.original,
+          lockAspectRatio: false,
+        ),
+        IOSUiSettings(
+          minimumAspectRatio: 1.0,
+        ),
+      ],
+    );
+
+    if (croppedFile != null) {
+      return File(croppedFile.path);
+    }
+    return null;
   }
 
   Future<void> _uploadImage() async {
@@ -179,11 +253,9 @@ class _PhotoCaptureScreenState extends State<PhotoCaptureScreen> {
         print("Upload success");
         print(response);
 
-        // Decode the JSON response
         final Map<String, dynamic> jsonResponse = json.decode(responseData);
         print(jsonResponse);
 
-        // Extract the filename and prediction
         final String prediction = jsonResponse['prediction'] as String;
 
         setState(() {
@@ -203,12 +275,13 @@ class _PhotoCaptureScreenState extends State<PhotoCaptureScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          title: Center(
-        child: Text(
-          'Smart Green',
-          textAlign: TextAlign.center,
+        title: Center(
+          child: Text(
+            'Smart Green',
+            textAlign: TextAlign.center,
+          ),
         ),
-      )),
+      ),
       body: Center(
         child: SingleChildScrollView(
           child: Column(
@@ -218,7 +291,7 @@ class _PhotoCaptureScreenState extends State<PhotoCaptureScreen> {
               SizedBox(height: 20),
               ElevatedButton(
                 onPressed: _pickImage,
-                child: Text('Capture Photo'),
+                child: Text('Capture and Crop Photo'),
               ),
               SizedBox(height: 10),
               ElevatedButton(
@@ -226,10 +299,7 @@ class _PhotoCaptureScreenState extends State<PhotoCaptureScreen> {
                 child: Text('Upload Photo'),
               ),
               SizedBox(height: 20),
-              _uploadedFileUrl != null
-                  // ? Image.network(_uploadedFileUrl!)
-                  ? Text("")
-                  : Text('No image uploaded.'),
+              _uploadedFileUrl != null ? Text("") : Text('No image uploaded.'),
               SizedBox(height: 20),
               _prediction != null
                   ? SizedBox(
